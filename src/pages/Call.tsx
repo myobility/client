e7ad83084848a26eb808abd90c385b51c1e85371
lee7198 <lee7198@naver.com>
2023-02-21 17:05:37 +0900

    fix: cors2

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -9,7 +9,7 @@ export const Call = () => {
   const [targetUid, setTargetUid] = useState<number | undefined>(undefined);
 
   const socket = io(
-    "https://port-0-server-node-luj2cle9ghnxl.sel3.cloudtype.app",
+    "https://port-0-server-node-luj2cle9ghnxl.sel3.cloudtype.app:3000",
     // "http://localhost:3000",
     {
       withCredentials: true,
